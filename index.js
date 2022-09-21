c88835db602f5d262dc000dcaa4ad720f5b6979f
Gaurab Das <gaurab2003das@gmail.com>
2022-09-21 10:33:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,60 +17,20 @@ const app = express();
 //   res.json({ status: "success" });
 // });
 
-// multiple fields upload
-// const upload = multer({ dest: "uploads/" });
 
-// const multiUpload = upload.fields([
-//   { name: "avatar", maxCount: 1 },
-//   { name: "resume", maxCount: 1 },
-// ]);
-// app.post("/upload", multiUpload, (req, res) => {
-//   console.log(req.files);
-//   res.json({ status: "success" });
-// });
-
-// custom filename
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads");
-//   },
-//   filename: (req, file, cb) => {
-//     const { originalname } = file;
-//     cb(null, `${uuid()}-${originalname}`);
-//   },
-// });
 
 const storage = multer.memoryStorage();
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.split("/")[0] === "image") {
-    cb(null, true);
-  } else {
-    cb(new multer.MulterError("LIMIT_UNEXPECTED_FILE"), false);
-  }
-};
 
-// ["image", "jpeg"]
 
 const upload = multer({
   storage,
-  fileFilter,
-  limits: { fileSize: 1000000000, files: 2 },
+
 });
-// app.post("/upload", upload.array("file"), async (req, res) => {
-//   try {
-//     const results = await s3Uploadv2(req.files);
-//     console.log(results);
-//     return res.json({ status: "success" });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
 
 app.post("/upload", upload.array("file"), async (req, res) => {
   try {
-    const results = await s3Uploadv3(req.files);
+    const results = await s3Uploadv2(req.files);
     console.log(results);
     return res.json({ status: "success" });
   } catch (err) {
@@ -78,26 +38,6 @@ app.post("/upload", upload.array("file"), async (req, res) => {
   }
 });
 
-app.use((error, req, res, next) => {
-  if (error instanceof multer.MulterError) {
-    if (error.code === "LIMIT_FILE_SIZE") {
-      return res.status(400).json({
-        message: "file is too large",
-      });
-    }
 
-    if (error.code === "LIMIT_FILE_COUNT") {
-      return res.status(400).json({
-        message: "File limit reached",
-      });
-    }
-
-    if (error.code === "LIMIT_UNEXPECTED_FILE") {
-      return res.status(400).json({
-        message: "File must be an image",
-      });
-    }
-  }
-});
 
 app.listen(5000, () => console.log("listening on port 5000"));
