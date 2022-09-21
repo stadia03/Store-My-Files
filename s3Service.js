d377a601886d394dcae8fa64424d6372312ae30c
Gaurab Das <gaurab2003das@gmail.com>
2022-09-21 10:32:59 +0530

    Update s3Service.js

--- s3Service.js
+++ s3Service.js
@@ -1,6 +1,6 @@
 const { S3 } = require("aws-sdk");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-const uuid = require("uuid").v4;
+
 
 exports.s3Uploadv2 = async (files) => {
   const s3 = new S3();
@@ -8,7 +8,7 @@ exports.s3Uploadv2 = async (files) => {
   const params = files.map((file) => {
     return {
       Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `uploads/${uuid()}-${file.originalname}`,
+      Key: `uploads/${file.originalname}`,
       Body: file.buffer,
     };
   });
@@ -16,18 +16,3 @@ exports.s3Uploadv2 = async (files) => {
   return await Promise.all(params.map((param) => s3.upload(param).promise()));
 };
 
-exports.s3Uploadv3 = async (files) => {
-  const s3client = new S3Client();
-
-  const params = files.map((file) => {
-    return {
-      Bucket: process.env.AWS_BUCKET_NAME,
-      Key: `uploads/${uuid()}-${file.originalname}`,
-      Body: file.buffer,
-    };
-  });
-
-  return await Promise.all(
-    params.map((param) => s3client.send(new PutObjectCommand(param)))
-  );
-};
